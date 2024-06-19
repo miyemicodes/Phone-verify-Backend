01604e880a591290e9912681794479771330492d
miyemicodes <lizzymiyemi@gmail.com>
2024-06-18 23:58:40 -0700

    first commit

---
+++ make_calls.js
@@ -0,0 +1,53 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+client.calls
+  .create({
+    url: "http://demo.twilio.com/docs/voice.xml",
+    to: "+123456789",
+    from: "+987654321",
+  })
+  .then((call) => console.log(call.sid));
+
+
+  {
+  "account_sid": "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "answered_by": null,
+  "api_version": "2010-04-01",
+  "caller_name": null,
+  "date_created": "Tue, 31 Aug 2010 20:36:28 +0000",
+  "date_updated": "Tue, 31 Aug 2010 20:36:44 +0000",
+  "direction": "inbound",
+  "duration": "15",
+  "end_time": "Tue, 31 Aug 2010 20:36:44 +0000",
+  "forwarded_from": "+141586753093",
+  "from": "+987654321",
+  "from_formatted": "+987654321",
+  "group_sid": null,
+  "parent_call_sid": null,
+  "phone_number_sid": "PNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "price": "-0.03000",
+  "price_unit": "USD",
+  "sid": "CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "start_time": "Tue, 31 Aug 2010 20:36:29 +0000",
+  "status": "completed",
+  "subresource_uris": {
+    "notifications": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Notifications.json",
+    "recordings": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Recordings.json",
+    "payments": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Payments.json",
+    "events": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Events.json",
+    "siprec": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Siprec.json",
+    "streams": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Streams.json",
+    "user_defined_message_subscriptions": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/UserDefinedMessageSubscriptions.json",
+    "user_defined_messages": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/UserDefinedMessages.json"
+  },
+  "to": "+123456789",
+  "to_formatted": "+123456789",
+  "trunk_sid": null,
+  "uri": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json",
+  "queue_time": "1000"
+}
